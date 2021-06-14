16ef81f99255c5e6af11bc5a31bcf224fe6846f3
Arijit Das <arijitdas18022006@gmail.com>
2021-06-14 15:39:27 +0530

    Update for ESP32C3 and ADXL sensor
    patch : major
    
    This update allows you to use and communicate any ADXL sensor with ESP32C3 and Edge Impulse.
    
    Co-Authored-By: Louis MOREAU <luis.omoreau@gmail.com>

--- ESP32-C3-Motion_inferencing/examples/nano_ble33_sense_accelerometer/esp32c3_adxl_accelerometer.ino
+++ ESP32-C3-Motion_inferencing/examples/nano_ble33_sense_accelerometer/esp32c3_adxl_accelerometer.ino
@@ -1,35 +1,20 @@
-/* Edge Impulse Arduino examples
- * Copyright (c) 2021 EdgeImpulse Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+/* Generated using Edge Impulse. 
+ * Modifications by Arijit Das(@arijitdas123student) and Louis Moreau)(@luisomoreau).
+ * This script has been modified for usage of ESP32-C3 and ADXLxxx sensors.
  */
-
 /* Includes ---------------------------------------------------------------- */
 #include <ESP32-C3-Motion_inferencing.h>
-#include <Arduino_LSM9DS1.h>
 
+// Pin connections with ESP32C3 and the ADXL sensor:
+const int groundpin = 18;             // analog input pin 4 -- ground
+const int powerpin = 19;              // analog input pin 5 -- voltage
+const int xpin = A3;                  // x-axis of the accelerometer
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis (only on 3-axis models)
 /* Constant defines -------------------------------------------------------- */
 #define CONVERT_G_TO_MS2    9.80665f
-
 /* Private variables ------------------------------------------------------- */
 static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
-
 /**
 * @brief      Arduino setup function
 */
@@ -37,21 +22,16 @@ void setup()
 {
     // put your setup code here, to run once:
     Serial.begin(115200);
-    Serial.println("Edge Impulse Inferencing Demo");
-
-    if (!IMU.begin()) {
-        ei_printf("Failed to initialize IMU!\r\n");
-    }
-    else {
-        ei_printf("IMU initialized\r\n");
-    }
-
+    Serial.println("Edge Impulse RISC-V Inferencing Demo");
+  pinMode(groundpin, OUTPUT);
+  pinMode(powerpin, OUTPUT);
+  digitalWrite(groundpin, LOW);
+  digitalWrite(powerpin, HIGH);
     if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != 3) {
         ei_printf("ERR: EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME should be equal to 3 (the 3 sensor axes)\n");
         return;
     }
 }
-
 /**
 * @brief      Printf function uses vsnprintf and output using Arduino Serial
 *
@@ -59,17 +39,14 @@ void setup()
 */
 void ei_printf(const char *format, ...) {
    static char print_buf[1024] = { 0 };
-
    va_list args;
    va_start(args, format);
    int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
    va_end(args);
-
    if (r > 0) {
        Serial.write(print_buf);
    }
 }
-
 /**
 * @brief      Get data and run inferencing
 *
@@ -78,27 +55,21 @@ void ei_printf(const char *format, ...) {
 void loop()
 {
     ei_printf("\nStarting inferencing in 2 seconds...\n");
-
     delay(2000);
-
     ei_printf("Sampling...\n");
-
     // Allocate a buffer here for the values we'll read from the IMU
     float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
-
     for (size_t ix = 0; ix < EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE; ix += 3) {
         // Determine the next tick (and then sleep later)
         uint64_t next_tick = micros() + (EI_CLASSIFIER_INTERVAL_MS * 1000);
-
-        IMU.readAcceleration(buffer[ix], buffer[ix + 1], buffer[ix + 2]);
-
-        buffer[ix + 0] *= CONVERT_G_TO_MS2;
-        buffer[ix + 1] *= CONVERT_G_TO_MS2;
-        buffer[ix + 2] *= CONVERT_G_TO_MS2;
-
+        buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3] = analogRead(xpin);
+        buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 2] = analogRead(ypin);
+        buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 1] = analogRead(zpin);
+        buffer[ix + 0] = analogRead(xpin);
+        buffer[ix + 1] = analogRead(ypin);
+        buffer[ix + 2] = analogRead(zpin);
         delayMicroseconds(next_tick - micros());
     }
-
     // Turn the raw buffer in a signal which we can the classify
     signal_t signal;
     int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
@@ -106,16 +77,13 @@ void loop()
         ei_printf("Failed to create signal from buffer (%d)\n", err);
         return;
     }
-
     // Run the classifier
     ei_impulse_result_t result = { 0 };
-
     err = run_classifier(&signal, &result, debug_nn);
     if (err != EI_IMPULSE_OK) {
         ei_printf("ERR: Failed to run classifier (%d)\n", err);
         return;
     }
-
     // print the predictions
     ei_printf("Predictions ");
     ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
@@ -128,7 +96,6 @@ void loop()
     ei_printf("    anomaly score: %.3f\n", result.anomaly);
 #endif
 }
-
 #if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_ACCELEROMETER
 #error "Invalid model for current sensor"
 #endif
